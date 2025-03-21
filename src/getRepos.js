de7ca4cbd13491675e4c2690af5b27be26572bfe
Ramon Asuncion <ramon@b1t.dev>
2025-03-21 09:38:09 -0400

    Get all the repos.

--- src/getRepos.js
+++ src/getRepos.js
@@ -14,30 +14,45 @@ async function initOctokit() {
 async function getPublicGithubRepos(username) {
   try {
     const responseData = [];
+    // https://docs.github.com/en/rest/using-the-rest-api/using-pagination-in-the-rest-api?apiVersion=2022-11-28
+    const perPage = 100;
+    let page = 1;
+    let hasMorePages = true;
 
     if (!octokit) {
       await initOctokit();
     }
 
-    const response = await octokit.request("GET /users/{username}/repos", {
-      username: username,
-      headers: {
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
-    });
-
-    const repos = response.data;
-
-    repos.forEach((repo) => {
-      responseData.push({
-        html_url: repo.html_url,
-        description: repo.description || "",
-        name: repo.name,
-        created_at: new Date(repo.created_at).getFullYear(),
-        language: repo.language || "N/A",
-        homepage: repo.homepage || "",
+    while (hasMorePages) {
+      const response = await octokit.request("GET /users/{username}/repos", {
+        username: username,
+        per_page: perPage,
+        page: page,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
       });
-    });
+
+      const repos = response.data;
+
+      if (repos.length === 0) {
+        hasMorePages = false;
+        break;
+      }
+
+      repos.forEach((repo) => {
+        responseData.push({
+          html_url: repo.html_url,
+          description: repo.description || "",
+          name: repo.name,
+          created_at: new Date(repo.created_at).getFullYear(),
+          language: repo.language || "N/A",
+          homepage: repo.homepage || "",
+        });
+      });
+
+      page++;
+    }
 
     responseData.sort((a, b) => b.created_at - a.created_at);
 
